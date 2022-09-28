c3511c80feb38173c9719e4c51811c0a079e6c59
Lovasz Benjamin <lovaszbenjamin@gmail.com>
2022-09-28 23:25:26 +0200

    Worked with one class, tried to understand the connection between the code and h2 db

--- src/main/java/com/library/benjaMainLib/model/Donor.java
+++ src/main/java/com/library/benjaMainLib/model/Donor.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class Donor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/library/benjaMainLib/repository/UserRepo.java
+++ src/main/java/com/library/benjaMainLib/repository/UserRepo.java
@@ -1,4 +1,14 @@
 package com.library.benjaMainLib.repository;
 
-public class LibraryRepo {
+import com.library.benjaMainLib.model.Donor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends CrudRepository<Donor, Integer>{
+    List<Donor> findAll();
+    Optional<Donor> findByUserId(Integer id);
 }
